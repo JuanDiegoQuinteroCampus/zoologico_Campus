a34873797acfaa32411a055cf1d52edbd810a483
VickyMontanezCampus <vmontanez707@gmail.com>
2023-08-27 02:37:22 -0500

    feat: :sparkles: Server
    Se levantó el servidor desde el app

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-a
+import express  from "express";
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+let config = JSON.parse(process.env.MY_CONFIG);
+
+app.listen(config, ()=> {
+    console.log(`http://${config.hostname}:${config.port}`);
+})
