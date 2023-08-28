72a708cee49143581aff2c42ee986165a444d768
VickyMontanezCampus <vmontanez707@gmail.com>
2023-08-28 16:28:07 -0500

    feat: :sparkles: Se añadio Cruds de la tabla Animales
    Se añadieron los Cruds get/post/put/delete de la tabla Animales

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 import express  from "express";
 import dotenv from "dotenv";
+import appAnimales from './routers/animales.js'
 dotenv.config();
 
 const app = express();
 let config = JSON.parse(process.env.MY_CONFIG);
+
+app.use(express.json());
+app.use("/animales", appAnimales);
 
 app.listen(config, ()=> {
     console.log(`http://${config.hostname}:${config.port}`);
