615208e3d579bbc25fd2ed91ae5ca694910b5a6a
VickyMontanezCampus <vmontanez707@gmail.com>
2023-08-29 13:18:19 -0500

    feat: :sparkles: Implentation Token
    Se implementaron los middlewares para el funcionamiento de tokens en cada colección

--- app.js
+++ app.js
@@ -8,20 +8,25 @@ import appIncidentes from "./routers/incidentes.js";
 import appMantenimiento from "./routers/mantenimiento.js";
 import appTipoAnimales from "./routers/tipo_animales.js";
 import appVisitantes from "./routers/visitantes.js";
+import appBodegas from "./routers/bodegas.js";
+import { appToken, appVerify } from "./helpers/token.js";
 dotenv.config();
 
 const app = express();
 let config = JSON.parse(process.env.MY_CONFIG);
 
+
 app.use(express.json());
-app.use("/animales", appAnimales);
-app.use("/areas", appAreas);
-app.use("/empleados", appEmpleados);
-app.use("/habitats", appHabitats);
-app.use("/incidentes", appIncidentes);
-app.use("/mantenimientos", appMantenimiento);
-app.use("/tipoAnimales", appTipoAnimales);
-app.use("/visitantes", appVisitantes);
+app.use("/token", appToken)
+app.use("/animales", appVerify, appAnimales);
+app.use("/areas", appVerify, appAreas);
+app.use("/bodegas", appVerify, appBodegas);
+app.use("/empleados", appVerify, appEmpleados);
+app.use("/habitats", appVerify, appHabitats);
+app.use("/incidentes", appVerify, appIncidentes);
+app.use("/mantenimientos", appVerify, appMantenimiento);
+app.use("/tipoAnimales", appVerify, appTipoAnimales);
+app.use("/visitantes", appVerify, appVisitantes);
 
 app.listen(config, ()=> {
     console.log(`http://${config.hostname}:${config.port}`);
