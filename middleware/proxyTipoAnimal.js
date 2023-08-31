bf4911f5ddeffbf127f7057b7cee144334ed6a8d
VickyMontanezCampus <vmontanez707@gmail.com>
2023-08-31 02:19:19 -0500

    feat: ✨ Middleware params of collection Tipo_Animal
    Se agregó una nueva función en el middleware de la colleción tipo_animal para validar los params enviados desde la uri

--- middleware/proxyTipoAnimal.js
+++ middleware/proxyTipoAnimal.js
@@ -1,70 +1,71 @@
 import 'reflect-metadata';
-import {plainToClass, classToPlain} from 'class-transformer';
-import {validate} from 'class-validator';
+import express from "express";
+import { plainToClass, classToPlain } from 'class-transformer';
+import { validate } from 'class-validator';
+import { validationResult } from 'express-validator';
 import { DTO } from "../helpers/token.js";
 import { Router } from "express";
-import express from "express";
-
 import { TipoAnimales } from '../dtocontroller/tipo_animales.js';
+import { parametro } from '../validator/params.js';
 
 const middlewareVerify = Router();
 const DTOData = Router();
-
 const proxyTipoAnimal = express();
-
+const middlewareParamTipAni = Router();
 
 proxyTipoAnimal.use(async (req, res, next) => {
-    try {
-      const data = plainToClass(TipoAnimales, req.body, { excludeExtraneousValues: true });
-      const validationErrors = await validate(data);
-      if (validationErrors.length > 0) {
-        const errors = validationErrors.map((err) => Object.values(err.constraints));
-        res.status(400).json({ message: "Validation error", errors });
-      } else {
-        req.body = JSON.parse(JSON.stringify(data));
-        next();
-      }
-    } catch (error) {
-      console.error("Internal server error:", error);
-      res.status(500).json({ message: "Internal server error", error: error.message });
+  try {
+    const data = plainToClass(TipoAnimales, req.body, { excludeExtraneousValues: true });
+    const validationErrors = await validate(data);
+    if (validationErrors.length > 0) {
+      const errors = validationErrors.map((err) => Object.values(err.constraints));
+      res.status(400).json({ message: "Validation error", errors });
+    } else {
+      req.body = JSON.parse(JSON.stringify(data));
+      next();
     }
-  });
-
-
-
-middlewareVerify.use((req,res,next) => {
-    if(!req.rateLimit) return; 
-    if (req.data && req.data.payload) {
-    let {payload} = req.data;
-    const { iat, exp, ...newPayload } = payload;
-    payload = newPayload;
-   
-    let Clone = JSON.stringify(classToPlain(plainToClass(DTO("tipoAnimales").class, {}, { ignoreDecorators: true })));
-
-    let Verify = Clone === JSON.stringify(payload);
-    
-    if (!Verify) {
-        return res.status(406).send({ status: 406, message: "No Autorizado" });
-    }
-}
-
-next();
+  } catch (error) {
+    console.error("Internal server error:", error);
+    res.status(500).json({ message: "Internal server error", error: error.message });
+  }
 });
 
-DTOData.use( async(req,res,next) => {
-    try {
-        let data = plainToClass(DTO("tipoAnimales").class, req.body);
-        await validate(data);
-        req.body = JSON.parse(JSON.stringify(data));
-        req.data = undefined;
-        next();
-    } catch (err) {
-        res.status(err.status).send(err)
+middlewareVerify.use((req, res, next) => {
+  if (!req.rateLimit) return;
+  if (req.data && req.data.payload) {
+    let { payload } = req.data;
+    const { iat, exp, ...newPayload } = payload;
+    payload = newPayload;
+    let Clone = JSON.stringify(classToPlain(plainToClass(DTO("tipoAnimales").class, {}, { ignoreDecorators: true })));
+    let Verify = Clone === JSON.stringify(payload);
+    if (!Verify) {
+      return res.status(406).send({ status: 406, message: "No Autorizado" });
     }
+  }
+  next();
+});
+
+DTOData.use(async (req, res, next) => {
+  try {
+    let data = plainToClass(DTO("tipoAnimales").class, req.body);
+    await validate(data);
+    req.body = JSON.parse(JSON.stringify(data));
+    req.data = undefined;
+    next();
+  } catch (err) {
+    res.status(err.status).send(err)
+  }
+});
+
+middlewareParamTipAni.use(parametro, (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) return res.status(400).json({ errors: errors.array() });
+  next();
 });
 
 export {
-    middlewareVerify,
-    DTOData,
-    proxyTipoAnimal
+  middlewareVerify,
+  DTOData,
+  proxyTipoAnimal,
+  middlewareParamTipAni
 };
