3fc42ddfad7fd5a3ffe14ae1d4df87c79de3dfd1
JuanDiegoQuinteroCampus <juanquinterocampus33@gmail.com>
2023-09-02 07:11:06 -0500

    feat: :sparkles: Consulta Empleados
    Se le añadio una consulta a la colection empleados que devuelve el area que le corresponde a los empleados por medio del id

--- api/v1/empleados.js
+++ api/v1/empleados.js
@@ -30,6 +30,38 @@ export async function getAllEmpleado(req, res) {
     }
 }
 
+export async function  getAreaPersonal(req, res) {
+    try {
+        let db = await con();
+    const id = Math.floor(req.params._id)
+    let collection = db.collection("empleados");
+    let result = await collection.aggregate([
+        {
+            $match: { _id: id }
+        },
+        {
+          $lookup: {
+            from: "areas",
+            localField: "_id",
+            foreignField: "_id",
+            as: "areas",
+          },
+        }
+         
+      ]).toArray();
+    if (!result || result.length === 0) {
+        res.status(404).json({
+            status: 404,
+            message: "Not Found"
+        });
+    } else {
+        res.send(result);
+    }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
 // appEmpleados.post("/post", middlewareVerify, proxyEmpeados, DTOData, async (req, res) => {
     
 // });
