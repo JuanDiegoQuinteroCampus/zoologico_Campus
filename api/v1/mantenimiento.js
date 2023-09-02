551a27bbe56fc14bd2f86b60237b2befa2d465c5
JuanDiegoQuinteroCampus <juanquinterocampus33@gmail.com>
2023-09-02 07:16:47 -0500

    feat: :sparkles: consulta mantenimiento
    Se le añadio una consulta a la collection mantenimiento lo cual por medio del id busca que empleado le corresponde ese mantenimiento

--- api/v1/mantenimiento.js
+++ api/v1/mantenimiento.js
@@ -24,6 +24,49 @@ export async function getAllMantenimientos(req, res) {
         res.send(result);
     }
 }
+export async function  getDataEmpleado(req, res) {
+    try {
+        let db = await con();
+    const id = Math.floor(req.params._id)
+    let collection = db.collection("mantenimiento");
+    let result = await collection.aggregate([
+        {
+            $match: { _id: id }
+        },
+        {
+          $lookup: {
+            from: "empleados",
+            localField: "_id",
+            foreignField: "_id",
+            as: "mantenimeinto_FK",
+          },
+        },
+         {
+          $project: {
+            "mantenimeinto_FK._id": 0,
+            "mantenimeinto_FK.id_area": 0,
+           
+      
+          },
+        },
+        {
+          $unwind: "$mantenimeinto_FK",
+        },
+         
+      ]).toArray();
+    if (!result || result.length === 0) {
+        res.status(404).json({
+            status: 404,
+            message: "Not Found"
+        });
+    } else {
+        res.send(result);
+    }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
 
 /* appMantenimiento.post("/post", middlewareVerify, proxyMantenimientos, DTOData, async (req, res) => {
     
