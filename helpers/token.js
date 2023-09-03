0c2a9ab1b2d256a25e4ea644e54950597b8993b5
VickyMontanezCampus <vmontanez707@gmail.com>
2023-09-03 17:51:00 -0500

    refactor: :recycle: Verificar token
    Se reciclo la función verificar token

--- helpers/token.js
+++ helpers/token.js
@@ -55,6 +55,20 @@ passport.use(new BearerStrategy(
     }
 ));
 
-appVerify.use("/", passport.authenticate('bearer', { session: false }));
+appVerify.use("/", async (req, res, next) => {
+    const { authorization } = req.headers;
+    if (!authorization) return res.status(400).send({ status: 400, token: "Falta Token de Autorización" });
+    try {
+        const encoder = new TextEncoder();
+        const jwtData = await jwtVerify(
+            authorization,
+            encoder.encode(process.env.JWT_PRIVATE_KEY)
+        );
+        req.data = jwtData;
+        next();
+    } catch (error) {
+        res.status(498).send({ status: 498, token: "Token Expirado" });
+    }
+})
 
 export { appToken, appVerify };
