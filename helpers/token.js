d6991cc1d7d02d20fc75c0222dcdf05e9b1edbf5
JuanDiegoQuinteroCampus <juanquinterocampus33@gmail.com>
2023-09-04 01:58:59 -0500

    feat: :ambulance: Introduce again Dto collections
    Se le corrigio el archivo a como estaba antes para apligar el login en otro momento y por ahora tener preparadas las versiones

--- helpers/token.js
+++ helpers/token.js
@@ -1,66 +1,59 @@
+import 'reflect-metadata';
 import dotenv from 'dotenv';
-import { con } from '../db/atlas.js';
+import { plainToClass, classToPlain } from 'class-transformer';
 import { Router } from 'express';
 import { SignJWT, jwtVerify } from 'jose';
-import passport from 'passport';
-import { Strategy as BearerStrategy } from 'passport-http-bearer';
-
+import { Animales } from '../dtocontroller/animales.js';
+import { Areas } from '../dtocontroller/areas.js';
+import { Bodegas } from '../dtocontroller/bodegas.js';
+import { Empleados } from '../dtocontroller/empleados.js';
+import { Habitats } from '../dtocontroller/habitats.js';
+import { Incidentes } from '../dtocontroller/incidentes.js';
+import { Mantenimientos } from '../dtocontroller/mantenimientos.js';
+import { TipoAnimales } from '../dtocontroller/tipo_animales.js';
+import { Visitantes } from '../dtocontroller/visitantes.js';
 dotenv.config();
-
 
 const appToken = Router();
 const appVerify = Router();
 
-appToken.post("/", async (req, res) => {
-    try {
-        const { username, password, id_rol } = req.body;
-        const conexionDB = await con();
-        const user = await conexionDB.collection('usuarios').findOne({ username: username });
-
-        if (!user || user.password !== password) {
-            return res.status(401).json({ status: 401, message: 'Credenciales inválidas' });
-        }
-        const id = user._id.toString();
-        const encoder = new TextEncoder();
-        const jwtConstructor = await new SignJWT({ id: id, username, password, id_rol })
-            .setProtectedHeader({ alg: 'HS256', typ: 'JWT' })
-            .setIssuedAt()
-            .setExpirationTime('1h')
-            .sign(encoder.encode(process.env.JWT_SECRET));
-        return res.status(200).json({ status: 200, jwt: jwtConstructor });
-    } catch (error) {
-        console.error('Error al generar el token:', error);
-        res.status(500).json({ status: 500, message: 'Error interno del servidor' });
-      }
-});
-
-passport.use(new BearerStrategy(
-    async function (token, done) {
-        try {
-            const encoder = new TextEncoder();
-            const jwtData = await jwtVerify(
-                token,
-                encoder.encode(process.env.JWT_SECRET)
-            );
-            if (!jwtData || !jwtData.payload || !jwtData.payload.id) {
-                return done(null, false);
-            }
-            const conexionDB = await con();
-            const usuario = await conexionDB.collection('usuarios').findOne({ _id: jwtData.payload.id });
-            if (!usuario) {
-                return done(null, false);
-            }
-            return done(null, usuario);
-        } catch (error) {
-            return done(error);
-        }
+const DTO = (cl) =>{
+    const match ={
+        'animales' : Animales,
+        'areas': Areas,
+        'bodegas': Bodegas,
+        'empleados': Empleados,
+        'habitats': Habitats,
+        'incidentes': Incidentes,
+        'mantenimientos': Mantenimientos,
+        'tipoAnimales': TipoAnimales,
+        'visitantes': Visitantes
     }
-));
+    const instan = match[cl];
+    if(!instan) throw { status:404, error: 'Not Found', message:'Token Invalido'}
+    return{atributos: plainToClass(instan,{},{ignoreDecorators: true}), class:instan}
+}
 
-appVerify.use("/", async (req, res, next) => {
-    const { authorization } = req.headers;
-    if (!authorization) return res.status(400).send({ status: 400, token: "Falta Token de Autorización" });
+appToken.use("/:collection", async(req,res)=>{
+    try {
+        let inst = DTO(req.params.collection).atributos;
+        const encoder = new TextEncoder();
+        const jwtconstructor = new SignJWT(Object.assign({}, classToPlain(inst)));
+        const jwt = await jwtconstructor
+        .setProtectedHeader({alg:"HS256", typ: "JWT"})
+        .setIssuedAt()
+        .setExpirationTime("30m")
+        .sign(encoder.encode(process.env.JWT_PRIVATE_KEY));
+        req.data = jwt;
+        res.status(201).send({status: 201, message:"Created", result: jwt});
+    } catch (e) {
+        res.status(404).send({status: 404, error:"Not Found", message: e.error});
+    }
+})
 
+appVerify.use("/", async(req,res,next)=>{
+    const {authorization} = req.headers;
+    if (!authorization) return res.status(400).send({status: 400, token: "Falta Token de Autorización"});
     try {
         const encoder = new TextEncoder();
         const jwtData = await jwtVerify(
@@ -68,23 +61,14 @@ appVerify.use("/", async (req, res, next) => {
             encoder.encode(process.env.JWT_PRIVATE_KEY)
         );
         req.data = jwtData;
-
-        const { id_rol } = jwtData.payload;
-
-        if (id_rol === 1) {
-            next();
-        } else if (id_rol === 2 && req.originalUrl.includes('/v2')) {
-            next();
-        } else {
-            return res.status(403).send({ status: 403, message: "No tienes permiso para acceder a esta versión" });
-        }
+        next();
     } catch (error) {
-        res.status(498).send({ status: 498, token: "Token Expirado" });
-        console.log(error)
+        res.status(498).send({status: 498, token: "Token Expirado"});
     }
-});
+})
 
-
-
-
-export { appToken, appVerify, passport };
+export {
+    appToken,
+    appVerify,
+    DTO
+};
